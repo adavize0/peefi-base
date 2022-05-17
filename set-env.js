7a099d1cf303987c21ad3421b96358163b347cbf
adavize0 <helloadavize@outlook.com>
2022-05-17 16:30:59 +0100

    fixed angular env files generation bug

--- set-env.js
+++ set-env.js
@@ -6,22 +6,23 @@ require('dotenv').config();
 
 // Read the command line arguments passed with yargs
 const environment = argv.environment;
-const isProduction = environment === 'prod';
-const targetPath = isProduction
-   ? `./src/environments/environment.prod.ts`
-   : `./src/environments/environment.ts`;
+const isProduction = () => environment === 'prod';
+const targetPaths = [`./src/environments/environment.prod.ts`, `./src/environments/environment.ts`];
 
 const environmentFileContent = `
 export const environment = {
-   production: ${isProduction},
+   production: ${isProduction()},
    OPENAI_KEY: "${process.env["OPENAI_KEY"]}",
 };
 `;
 
 // Write the content to the respective file
-writeFile(targetPath, environmentFileContent, function (err) {
-   if (err) {
-      console.log(err);
-   }
-   console.log(`Written .env variables to ${targetPath}`);
-})
+for(let path of targetPaths){
+  writeFile(path, environmentFileContent, function (err) {
+    if (err) {
+       throw(err)
+    }
+    console.log(`Written .env variables to ${path}`);
+ })
+
+}
